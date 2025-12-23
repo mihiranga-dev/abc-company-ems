776bdb083e98aec42abde8f3487a953a70323da6
mihiranga-dev <mihiranga.dev@gmail.com>
2025-12-23 20:03:26 +0530

    Created Package structure and DatabaseConfig for dual-database routing

---
+++ ems-backend/src/main/java/com/abccompany/ems_backend/config/DatabaseConfig.java
@@ -0,0 +1,12 @@
+package com.abccompany.ems_backend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+@Configuration
+@EnableJpaRepositories(basePackages = "com.abccompany.ems_backend.repository.oracle")
+@EnableMongoRepositories(basePackages = "com.abccompany.ems_backend.repository.mongo")
+public class DatabaseConfig {
+
+}
